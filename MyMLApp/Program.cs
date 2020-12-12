62d786627eecd56ab215960febe3b0502e4ad27c
Sam <109807@st-maartenscollege.nl>
2020-12-12 15:41:40 +0100

    Add project files.

---
+++ MyMLApp/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using MyMLAppML.Model;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String varAntwoord = "ja";
+            while (varAntwoord == "ja")
+            {
+                Console.WriteLine("geef een zin");
+                String varZin = Console.ReadLine();
+                Console.WriteLine(varZin);
+
+                // Add input data
+                var input = new ModelInput();
+                input.SentimentText = varZin;
+
+                // Load model and predict output of sample data
+                ModelOutput result = ConsumeModel.Predict(input);
+                Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+
+                Console.WriteLine("Wil je een zin testen of deze toxic is of niet? Zo ja, antwoord ja");
+                varAntwoord = Console.ReadLine();
+
+            }
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
